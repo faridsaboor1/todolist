d3974e66a6d845ed2e2abf855adae91dc03f9b04
farid saboor <134936000+faridsaboor1@users.noreply.github.com>
2023-12-24 16:16:47 +0330

    add style

--- tpl/update_tpl.php
+++ tpl/update_tpl.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>update task</title>
+    <link rel="stylesheet" href="assets/css/update_style.css">
 </head>
 <body>
-    <form action="update.php?update_task=<?= $_GET['update_task'] ?>" method="post">
-        <input type="text" name="upTitle" value="<?= $task_data->title ?? '' ?>">
-        <input type="submit" name="send">
+    <form action="update.php?update_task=<?= $_GET['update_task'] ?>" method="post" >
+        <input type="text" name="upTitle" value="<?= $task_data->title ?? '' ?>" class="inp_up">
+        <input type="submit" name="send" class="btn_up">
     </form>
 </body>
 </html>
